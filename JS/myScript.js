d02713a12e33612a35e3de47a8ed3165d1a14b0d
Bárbara Alfaro <barbaraabaraqui@gmail.com>
2021-03-16 21:28:26 -0300

    orden

---
+++ JS/myScript.js
@@ -0,0 +1,32 @@
+///modal del carrito///
+$( document ).ready(function(){
+    console.log("JQuery loaded");
+
+    // var buttons = document.getElementsByTagName('button')
+    // buttons era una lista
+    // for o foreach 
+    // element.addEventListeenr('click', funciont(){})
+
+    // Acceso con tag element
+    $("button").click(botonPresionado);
+    
+    // Acceso con class name
+    $(".btn-sm").click(function (){ 
+        console.log("elimino un item");
+        $("#cartModal").hide(); // acceso con ID
+        
+    });
+
+
+
+});
+
+function botonPresionado(){
+    console.log("Buttone pressed");
+}
+function sumar(){
+
+    var num1 = 5;
+    var num2 = 8;
+   suma = num1 + num2;
+ }
