5cc4e7ec9a7e6f7323787b2d0f49c4762a762152
Bárbara Alfaro <barbaraabaraqui@gmail.com>
2021-03-21 17:09:26 -0300

    cambios

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,20 @@
-console.log('hola js');
+/*solucion de bluuweb
+Document.addEventListener('DOMContentLoaded', () => {
+  fetchData()
+})
+
+const fetchData = async () => {
+  try{
+    const res = await fetch('api.json')    
+    const data = await res.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+*/
+document.addEventListener('DOMContentLoaded', () => {
+	articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+	insertarCarritoHTML();
+})
