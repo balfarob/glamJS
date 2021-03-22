d284dc47e26900636d87d8cc35419c0f40747e0c
Bárbara Alfaro <barbaraabaraqui@gmail.com>
2021-03-22 20:44:32 -0300

    cambios nuevamente

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,11 @@
-/*fetch('https://regres.in/api/users')
+ /*fetch('https://regres.in/api/users')
   .then( res => res.json())
   .then(data => console.log(data))*/
 /* La forma que enseñó el profesor*/
+/*Let = fetch('api.json')
+.then( res => res.json())
+.then(datos => console.log(datos))*/
+/*Intento numero 10000*/
+$("button").click(funtion(){
+  
+})
