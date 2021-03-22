800c3d04defdf845afb6957673a951638fe51043
Bárbara Alfaro <barbaraabaraqui@gmail.com>
2021-03-21 21:46:33 -0300

    cambios

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,6 @@
-/*solucion de bluuweb
-Document.addEventListener('DOMContentLoaded', () => {
-  fetchData()
-})
-
-const fetchData = async () => {
-  try{
-    const res = await fetch('api.json')    
-    const data = await res.json()
-    console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
-}
+/*
+Acceder a la data con fetch API, utilizando AJAX
 */
-document.addEventListener('DOMContentLoaded', () => {
-	articulosCarrito = JSON.parse(localStorage.getItem('carrito')) || [];
-
-	insertarCarritoHTML();
-})
+fetch('api.json')
+  .then( res =>res.json())
+  .then(data => console.log(data))
